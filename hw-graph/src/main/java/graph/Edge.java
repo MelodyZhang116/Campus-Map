64d44672b54d25658d56c4a3eb598ad215a4feb5
11691 <mzhsng22@cs.washington.edu>
2021-04-29 23:28:42 +0800

    hw5 part3 finished

--- hw-graph/src/main/java/graph/Edge.java
+++ hw-graph/src/main/java/graph/Edge.java
@@ -1,4 +1,20 @@
 package graph;
 
-public class Edge {
+/**
+ * <b>Edge</b> represents <b>immutable</b> directed edge that starts from one location
+ * to another location, with a name with it. Assume that no two locations have two edges
+ * with the same name and same direction.
+ * The location that the edge starts from is parent, and the location that the edge
+ * end with is child.
+ */
+
+public final class Edge {
+
+    /**
+     * construct a new Edge from parent to child with a name
+     * @param parent the edge starts from
+     * @param child the edge ends with
+     * @param label the edge's name
+     */
+    public Edge(String parent,String child,String label){}
 }
