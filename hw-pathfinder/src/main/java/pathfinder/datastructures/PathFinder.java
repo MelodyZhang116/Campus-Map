4d95494de143c4b97cd290844c448a1cdfe8a0ca
11691 <mzhsng22@cs.washington.edu>
2021-05-21 15:00:40 +0800

    hw5-part2-final

--- hw-pathfinder/src/main/java/pathfinder/datastructures/PathFinder.java
+++ hw-pathfinder/src/main/java/pathfinder/datastructures/PathFinder.java
@@ -5,14 +5,53 @@ import graph.Graph;
 import java.util.*;
 
 
-
+/**
+ * This class is used to find the shorted path using Dijkstra's Algorithm.
+ * @param <A> the type of node(undecided, can be number, string...)
+ * @param <Number> the type that is stored as label of edge in graph
+ */
 public class PathFinder<A,Number> {
-    Graph<A,Double> g;
+    public static final boolean DEBUG = false;
 
+    // the graph containing all the info of location and distance
+    Graph<A,Double> g;
+    //AF: find the path that start from a node of this.g and end with a node from this.g
+    //    and has the shortest distance(the sum of distance of edges in this.g is smallest)
+    //RepInv: g is not null and for each term in this.g, there is no null.
+    //        for each edge, its label is not negative.
+
+    /**
+     * construct a new PathFinder
+     * @param graph that searched for path
+     */
     public PathFinder(Graph<A,Double> graph){
         g = graph;
     }
 
+    private void checkRep(){
+        if(DEBUG){
+            assert(!g.equals(null)):"graph is null";
+            assert(!g.containsNode(null)): "The graph has null node(s)";
+            assert (!g.allEdges().contains(null)) :"The graph has null edge(s)";
+
+            for(List<Graph.Edge<A, Double>> list:g.allEdges()){
+                for(Graph.Edge<A,Double> ed:list){
+                    assert(ed != null):"The graph has null edges";
+                    assert(ed.getName()>=0.0):"The cost of edge is negative";
+                }
+
+            }
+        }
+    }
+
+    /**
+     * find the shortest path ( the smallest sum of cost of segment)
+     * @param starting path start from
+     * @param destination path end with
+     * @return shortest path from starting to destination
+     * @spec.requires starting != null && destination != null &&
+     * starting exist and destination exist
+     */
     public Path<Graph.Node<A>> findPath(A starting, A destination){
         if(!g.containsNode(starting) || !g.containsNode(destination)){
             throw new IllegalArgumentException();
@@ -21,19 +60,21 @@ public class PathFinder<A,Number> {
         Graph.Node<A> dest = new Graph.Node<A>(destination);
 
         Comparator<Path<Graph.Node<A>>> comparator= new CostComparator();
-        Queue<Path<Graph.Node<A>>> active = new PriorityQueue<Path<Graph.Node<A>>>(comparator);// Each element is a path from start to a given node
+        Queue<Path<Graph.Node<A>>> active = new PriorityQueue<Path<Graph.Node<A>>>(comparator);
+        // Each element is a path from start to a given node
         // A path's priority in the queue is the total cost of the path
-        List<Graph.Node<A>> finished = new ArrayList<Graph.Node<A>>(); // set of A for which we know the minimum-cost path from start
+        List<Graph.Node<A>> finished = new ArrayList<Graph.Node<A>>();
+        // set of A for which we know the minimum-cost path from start
         active.add(new Path<Graph.Node<A>>(start));
 
         while(!active.isEmpty()){
             Path<Graph.Node<A>> minPath = active.poll();
             Graph.Node<A> minDest = minPath.getEnd();
             if(minDest.equals(dest)){
-                return minPath;
+                return minPath; //find you!
             }
             if(!finished.contains(minDest)){
-                for(Graph.Edge<A,Double> ed:g.listChildren(minDest.getName())){
+                for(Graph.Edge<A,Double> ed:g.listChildren(minDest.getName())){ // go through all edges that start from minDest
                     if(!finished.contains(ed.getChild())){
                         Path<Graph.Node<A>> newPath = minPath.extend(ed.getChild(),ed.getName());
                         active.add(newPath);
@@ -51,7 +92,9 @@ public class PathFinder<A,Number> {
     }
 
 
-
+    /**
+     * a comparator that compare the total cost of path
+     */
     public class CostComparator implements Comparator<Path<Graph.Node<A>>>{
         @Override
         public int compare(Path<Graph.Node<A>> x,Path<Graph.Node<A>> y ){
