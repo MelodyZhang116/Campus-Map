1519678cd798702e7c57422d0ff192cf32900fc1
11691 <mzhsng22@cs.washington.edu>
2021-06-02 14:31:38 +0800

    hw5-part2-final

--- hw-campuspaths-server/src/main/java/campuspaths/SparkServer.java
+++ hw-campuspaths-server/src/main/java/campuspaths/SparkServer.java
@@ -1,4 +1,4 @@
-/*
+package campuspaths;/*
  * Copyright (C) 2021 Kevin Zatloukal.  All rights reserved.  Permission is
  * hereby granted to students registered for University of Washington
  * CSE 331 for use solely during Spring Quarter 2021 for purposes of
@@ -8,9 +8,19 @@
  * interested in reusing these course materials should contact the
  * author.
  */
+import com.google.gson.Gson;
+import graph.Graph;
+import pathfinder.CampusMap;
+import pathfinder.datastructures.Path;
+import pathfinder.datastructures.Point;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
 
-package campuspaths;
-
+import java.util.ArrayList;
+import java.util.List;
+import campuspaths.*;
 import campuspaths.utils.CORSFilter;
 
 public class SparkServer {
@@ -18,10 +28,27 @@ public class SparkServer {
     public static void main(String[] args) {
         CORSFilter corsFilter = new CORSFilter();
         corsFilter.apply();
-        // The above two lines help set up some settings that allow the
-        // React application to make requests to the Spark server, even though it
-        // comes from a different server.
-        // You should leave these two lines at the very beginning of main().
+        CampusMap map = new CampusMap();
+
+        Spark.get("/shortestPath", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                String startShortName = request.queryParams("startShortName");
+                String endShortName = request.queryParams("endShortName");
+                if(startShortName == null || endShortName == null) {
+                    // You can also have a message in "halt" that is displayed in the page.
+                    Spark.halt(400, "must have start and end");
+                }
+                if(!map.shortNameExists(startShortName) || !map.shortNameExists(endShortName)){
+                    Spark.halt(400, "start and end must be valid short name on campus");
+                }
+                Path<Graph.Node<Point>> path = map.findShortestPath(startShortName,endShortName);
+
+                Gson gson = new Gson();
+                String jsonResponse = gson.toJson(path);
+                return jsonResponse;
+            }
+        });
 
         // TODO: Create all the Spark Java routes you need here.
     }
